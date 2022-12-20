1a47d50910bc0dbcb63b4167530608e2f39479fa
Pedro_Gabriel <75584882+Pedro-gabriel@users.noreply.github.com>
2022-12-20 15:03:31 -0300

    update - Definindo palavra

--- assets/app.js
+++ assets/app.js
@@ -41,6 +41,29 @@ function exibeCategoria(){
     categoria.innerHTML = retornaCategoria();
 }
 
+function retornaNumAleatorio(max){
+    return Math.floor(Math.random() * max);
+}
+
+function definePalavraProposta(){
+    const arrayPalavras = categorias[categoria.innerHTML];
+    let indicePalavra = retornaNumAleatorio(arrayPalavras.length);
+    palavraProposta = arrayPalavras[indicePalavra];
+    console.log(palavraProposta);
+    ocultaPalavra();
+}
+
+function ocultaPalavra(){
+    let palavraOcultada = "";
+    for(let i = 0; i < palavraProposta.length; i++){
+        palavraOcultada+="-";
+    }
+    exibePalavraInterface(palavraOcultada);
+}
+
+function exibePalavraInterface(palavra){
+    palavraInterface.innerHTML = palavra;
+}
 
 /*
 Recebe o evento do teclado e passa apenas o valor da letra para a função tentativa
@@ -87,6 +110,7 @@ function iniciaJogo(){
     letrasErradasArray = [];
     ocultaBoneco();
     exibeCategoria();
+    definePalavraProposta();
     letrasErradas.innerHTML = "Letras erradas: ";
     window.addEventListener("keypress", retornaLetra);
 }
